2ca3329bc6eafc8d98f5bc999e5276e195f15725
zombifications <imma@terrorist.live>
2021-07-17 07:11:52 -0400

    help command changes, renamed 'Cogs' to 'cogs', added requirements.txt

--- Cogs/core/help.py
+++ Cogs/core/help.py
@@ -6,7 +6,7 @@ import discord
 import datetime
 import contextlib
 
-class HelpEmbed(discord.Embed): # Our embed with some preset attributes to avoid setting it multiple times
+class HelpEmbed(discord.Embed):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		self.timestamp = datetime.datetime.utcnow()
@@ -29,7 +29,7 @@ class Help(commands.HelpCommand):
 
 	async def send_bot_help(self, mapping):
 		ctx = self.context
-		embed = HelpEmbed(title="{} Help Map".format(ctx.me.name))
+		embed = HelpEmbed()
 		usable = 0
 
 		for cog, commands in mapping.items():
@@ -40,42 +40,39 @@ class Help(commands.HelpCommand):
 					name = cog.qualified_name
 					description = cog.description or "No description"
 				else:
-					name = "No Category"
-					description = "Commands with no category"
+					name = "Not Sorted"
+					description = "Commands that are not sorted into a category"
 
-				embed.add_field(name=f"{name} Category [{amount_commands}]", value=description)
+				embed.add_field(name=f"{name} [{amount_commands}]", value=description, inline=False)
 
-		embed.description = f"{len(ctx.bot.commands)} commands | {usable} usable" 
+		embed.description = f"{usable} commands" 
 		await self.send(embed=embed)
 
 	async def send_command_help(self, command):
 		signature = self.get_command_signature(command)
-		embed = HelpEmbed(title=signature, description=command.help or "No help found...")
+		embed = HelpEmbed(title=signature, description=command.help or "No Help Specified By Developer")
 
 		if cog := command.cog:
-			embed.add_field(name="Category", value=cog.qualified_name)
+			embed.add_field(name="Category", value=cog.qualified_name, inline=False)
 
 		can_run = "No"
 		with contextlib.suppress(commands.CommandError):
 			if await command.can_run(self.context):
 				can_run = "Yes"
 			
-		embed.add_field(name="Usable", value=can_run)
+		embed.add_field(name="Enabled?", value=can_run, inline=False)
 
 		if command._buckets and (cooldown := command._buckets._cooldown):
-			embed.add_field(
-				name="Cooldown",
-				value=f"{cooldown.rate} per {cooldown.per:.0f} seconds",
-			)
+			embed.add_field(name="Cooldown", value=f"{cooldown.rate} per {cooldown.per:.0f} seconds", inline=False)
 
 		await self.send(embed=embed)
 
 	async def send_help_embed(self, title, description, commands):
-		embed = HelpEmbed(title=title, description=description or "No help found...")
+		embed = HelpEmbed(title=title, description=description or "No Description Specified By Developer")
 
 		if filtered_commands := await self.filter_commands(commands):
 			for command in filtered_commands:
-				embed.add_field(name=self.get_command_signature(command), value=command.help or "No help found...")
+				embed.add_field(name=self.get_command_signature(command), value=command.help or "No Help Specified By Developer")
 		   
 		await self.send(embed=embed)
 
@@ -85,7 +82,7 @@ class Help(commands.HelpCommand):
 
 	async def send_cog_help(self, cog):
 		title = cog.qualified_name or "No"
-		await self.send_help_embed(f'{title} Category', cog.description, cog.get_commands())
+		await self.send_help_embed(title, cog.description, cog.get_commands())
 
 def setup(bot):
 	bot.help_command = Help()
